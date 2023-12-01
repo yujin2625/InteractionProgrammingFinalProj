81c03c4e0071eba00ddfbbb820bd755227807d9e
yujin <yujin2625@gmail.com>
2023-12-01 20:08:27 +0900

    scene 기초

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class GameManager : MonoBehaviour
+{
+
+
+
+}
---
+++ Assets/Scripts/Manager/UIController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIController : MonoBehaviour
+{
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
