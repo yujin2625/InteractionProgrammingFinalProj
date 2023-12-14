7de8c1bb4ec4880eed38428f885db1e9b4049bc7
yujin <yujin2625@gmail.com>
2023-12-14 23:44:05 +0900

    commit

--- Assets/Scripts/Manager/SceneManager.cs
+++ Assets/Scripts/Manager/SceneManager.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 
 public partial class GameManager
 {
-    string currentScene;
-    string[] Scenes;
-    void LoadNextScene()
-    {
-        
-    }
+    SceneManager SceneManager;
 
+    public string currentScene;
+    public string[] Scenes;
+    Queue<string> scene_queue;
+    private void Start()
+    {
+        foreach (string scene in Scenes ){
+            scene_queue.Enqueue(scene);
+        }
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(scene_queue.Dequeue());
+        Scenes = Array.Empty<string>();
+        Scenes = scene_queue.ToArray();
+    }
 
 }
